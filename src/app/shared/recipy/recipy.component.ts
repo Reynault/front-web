62bf5e8a0a462169319847190bdb81a77786bec2
Reynault Sies <reynault.sies@gmail.com>
2020-11-19 01:00:50 +0100

    Ajout des éléments de l'architecture

---
+++ src/app/shared/recipy/recipy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-recipy',
+  templateUrl: './recipy.component.html',
+  styleUrls: ['./recipy.component.css']
+})
+export class RecipyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
