4879a84fcc4ecb197fbfcbb6031d7d2ed065974a
Reynault Sies <reynault.sies@gmail.com>
2020-11-19 01:00:08 +0100

    Configuration de la partie Front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { InscriptionComponent } from './inscription/inscription.component';
+import { ConnectionComponent } from './connection/connection.component';
+import { DialogComponent } from './shared/dialog/dialog.component';
+import { RecipyComponent } from './shared/recipy/recipy.component';
+import { RecipiesComponent } from './recipies/recipies.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import {AppRoutingModule} from './app-routing.module';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    InscriptionComponent,
+    ConnectionComponent,
+    DialogComponent,
+    RecipyComponent,
+    RecipiesComponent,
+    NavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
